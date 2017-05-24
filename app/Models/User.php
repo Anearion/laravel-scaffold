49967fdae8f843ae166dd6c9d3ec8c0cde91d70e
vagrant <vagrant@homestead>
2017-05-24 15:13:39 +0000

    sistemata la gestione dei ruoli

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Zizaco\Entrust\Traits\EntrustUserTrait;
+use App\Models\Role;
 
 class User extends Authenticatable
 {
@@ -27,4 +28,5 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+  
 }
---
+++ resources/views/administration/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.blank')
+
+@push('stylesheets')
+<!-- Example -->
+<!--<link href=" <link href="{{ asset("css/myFile.min.css") }}" rel="stylesheet">" rel="stylesheet">-->
+@endpush
+
+@section('main_container')
+
+    <!-- page content -->
+    <div class="right_col" role="main">
+        <div class="col-md-12">
+            <h1>Federlegno Administration</h1>
+        </div>
+    </div>
+    <!-- /page content -->
+
+    <!-- footer content -->
+    <footer>
+        <div class="pull-right">
+            Piattaforma Federlegno by <a href="https://colorlib.com">Cloud</a>
+        </div>
+        <div class="clearfix"></div>
+    </footer>
+    <!-- /footer content -->
+@endsection
