6739e37efe7aea2c80b10e7d38d332ded5d6e21c
vagrant <vagrant@homestead>
2017-05-24 12:49:37 +0000

    inserita migration per le aziende

---
+++ database/migrations/2017_05_24_120822_create_companies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->string('business_name');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('vat_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
